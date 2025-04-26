a8eb579460a916ac4256bbed329d6a9e8cf406eb
Özgür Emre YILMAZ <37869696+ylmazozgur@users.noreply.github.com>
2025-04-26 17:43:45 +0300

    Add files via upload

---
+++ Hafta2/degiskenler.py
@@ -0,0 +1,114 @@
+x = 5 + 5
+x
+
+y = 5.1
+y 
+
+ali = " ", ' '
+ali = "blablablabla"
+ali
+type(ali)
+
+ali = " ali bana bilmem  'blbalbalbalba' dedi."  
+ali = ' ali bana bilmem  "blbalbalbalba" dedi.' 
+ali = " ali bana bilmem \"blbalbalbalba \" dedi." 
+
+bool -> True, False
+gulay = True
+1
+type(gulay)
+gulay = 1
+
+type(gulay)
+
+x = float(5)
+x
+
+muhammed = False
+muhammed
+
+print(type(1 == 5))
+
+*, /, //, -, **
+
+x = 4; y = 7; z = "ağaç"
+x,y,z = 4, 7, "ağaç"
+
+x-y
+x/y
+x//y
+y/x
+y//x
+x**y
+4**2
+z*3
+z =  "ağaç"
+
+
+
+vize = 20
+vizeSon
+
+final = 30
+finalSon
+
+vizeSon + finalSon
+gectiKaldi = "geçti"
+
+
+vize = 20
+final = 30
+vizeSon = 20*0.40 
+finalSon = 30*0.60
+sonuc = vizeSon + finalSon
+if sonuc < 50:
+    print("kaldı")
+else:
+    print("geçti")
+print(sonuc)
+print("geçti")
+
+
+<, > , =, ==, <=, >=, !==
+
+5 > 6
+6 < 5
+6 = 5
+6 == 5
+6 <= 5
+6 >= 5
+6 != 5
+
+if ......<,>,!=,==,
+if not 6 < 5:
+    kasjdaksdasdas
+else:
+    sdfjkhsjkfhsdjkf
+
+if 5 < 10:
+    sjadnasjkdasdas
+elif 5 > 9:
+    askjdsajdjaskdsa
+else:  
+    askdjaslkd 
+
+x = 1
+if x > 10:
+    print(x*5)
+elif x < 10:
+    print(x/8)
+else:
+    print(x)
+
+
+del x
+
+x = 1
+if x > 10:
+    print(x * 5)
+elif x < 10:
+    print(x / 8)
+else:
+    print(x)
+
+
---
+++ Hafta2/degiskenler2.py
@@ -0,0 +1,17 @@
+x = 1
+if x > 10:
+    print(x * 5)
+else:
+    print(x*2)
+
+
+
+vize = 20
+final = 30
+vizeSon = 20*0.40 
+finalSon = 30*0.60
+sonuc = vizeSon + finalSon
+if sonuc < 50:
+    print("kaldi")
+else:
+    print("geçti")
